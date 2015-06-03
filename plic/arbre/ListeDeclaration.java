2364160f0302e7abefc805a3872afda7dce39ed8
Achyle <achyle03@gmail.com>
2015-06-03 18:46:17 +0200

    ajout classe listeDeclaration

---
+++ plic/arbre/ListeDeclaration.java
@@ -0,0 +1,30 @@
+package plic.arbre;
+
+import java.util.ArrayList;
+
+public class ListeDeclaration {
+	
+	ArrayList<Declaration> listeDeclaration;
+	
+	public ListeDeclaration(){
+		this.listeDeclaration = new ArrayList<Declaration>();
+	}
+	
+	public void ajouterAction(Declaration declaration){
+		this.listeDeclaration.add(declaration);
+	}
+	
+	public String generer(){
+		StringBuilder strbr = new StringBuilder();
+		strbr.append("# zone de reservation de memoire\n\n");
+		strbr.append("	// initialise s7 avec sp \n"); 
+		strbr.append("	la $s7,($sp) \n\n");
+		//for(int i=0;i<Tds.getInstance().getTailleMap();i++){	
+			//strbr.append("	add $sp ,$sp,-4 \n");
+		//}
+		strbr.append("\n# zone programme\n");;
+		for(Declaration decl : declaration)
+			strbr.append(decl.generer()+"\n");
+		return strbr.toString();
+	}
+}
